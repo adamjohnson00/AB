b7adbd8a3bdbb2bef05c0930071da6734efd0a3e
Adam Johnson <adam.github@outlook.com>
2015-06-24 10:02:34 -0500

    Font size change.

--- ab/GameScene.swift
+++ ab/GameScene.swift
@@ -15,7 +15,7 @@ class GameScene: SKScene
         // commeeeeeeennnnttttt
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
+        myLabel.fontSize = 75;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
         self.addChild(myLabel)
